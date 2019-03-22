8b0ff25a03a16ca564e85f66527d93eb557bfe75
ZiRo Dev <31284099+Clientik@users.noreply.github.com>
2019-03-22 23:15:13 +0300

    V1.0

---
+++ js/main.js
@@ -0,0 +1,65 @@
+﻿function startTimer() {
+    var my_timer = document.getElementById("my_timer");
+    var time = my_timer.innerHTML;
+    var arr = time.split(":");
+    var h = arr[0];
+    var m = arr[1];
+    var s = arr[2];
+    if (s == 0) {
+      if (m == 0) {
+        if (h == 0) {
+          alert("Время вышло!");
+          window.location.reload();
+          return;
+        }
+        h--;
+        m = 60;
+        if (h < 10) h = "0" + h;
+      }
+      m--;
+      if (m < 10) m = "0" + m;
+      s = 59;
+    }
+    else s--;
+    if (s < 10) s = "0" + s;
+    document.getElementById("my_timer").innerHTML = h+":"+m+":"+s;
+    setTimeout(startTimer, 1000);
+  }
+  //Функция показа
+			function show(state){
+
+					document.getElementById('window').style.display = state;			
+					document.getElementById('wrap').style.display = state; 			
+			}
+			 function check_it() {
+    var testing = document.getElementById("checking").value;
+    var rex = /^([a-z0-9_\-]+\.)*[a-z0-9_\-]+@([a-z0-9][a-z0-9\-]*[a-z0-9]\.)+[a-z]{2,4}$/i; //регулярное выражение для e-mail
+    if(rex.test(testing)){ alert("На вашу почту будет отправлена ссылка с оплатой!");
+	show('none');
+	document.getElementById('checking').style.border = 'none';
+	}else{
+			alert("Введите корректный e-mail!");
+			document.getElementById('checking').style.border = '1px solid red';
+			}
+	
+}
+$(window).on('load', function () {
+    var $preloader = $('#page-preloader'),
+        $spinner   = $preloader.find('.spinner');
+    $spinner.fadeOut();
+    $preloader.delay(1000).fadeOut('slow');
+});
+ $(function(){
+    $('a[href*=#]').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+        && location.hostname == this.hostname) {
+            var $target = $(this.hash);
+            $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+            if ($target.length) {
+                var targetOffset = $target.offset().top;
+                $('html,body').animate({scrollTop: targetOffset}, 500);//скорость прокрутки
+                return false;
+            }
+        }
+    });
+});
